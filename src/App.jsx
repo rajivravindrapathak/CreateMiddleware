eb157c562deca26f3c2d91d0c4ccd439f3ece450
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-09-20 18:36:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Middleware from './Components/Middleware'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Middleware />
+    </div>
+  )
+}
+
+export default App
